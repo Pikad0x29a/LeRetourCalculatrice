109a84c12a573691286e304dbce60d770a377090
Pikad0x29a <pikad0x29a.root@proton.me>
2025-01-27 16:22:55 +0100

    Modification README.md et Calcul.java

--- src/Calcul.java
+++ src/Calcul.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+
+// Product
 public class Calcul {
+    private ArrayList<IElemCal> liste;
+
+    private Calcul(CalculBuilder builder) {
+        this.liste = builder.liste;
+    }
+
+    public ArrayList<IElemCal> getListe() {
+        return this.liste;
+    }
+
+    public String toString() {
+        String result = "";
+        for (IElemCal item : this.liste) {
+            result += item + ", ";
+        }
+
+        return result;
+    }
+
+    // ConcreteBuilder
+    public static class CalculBuilder {
+        private ArrayList<IElemCal> liste = new ArrayList<>();
+
+
+        public CalculBuilder addItemListe (IElemCal item) {
+            this.liste.add(item);
+            return this;
+        }
+
+        public Calcul build() {
+            return new Calcul(this);
+        }
+    }
 }
